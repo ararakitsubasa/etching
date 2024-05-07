809e3f8eb9e2ee80c968eedfa378af3bf3a065ab
ararakitsubasa <67034838+ararakitsubasa@users.noreply.github.com>
2024-05-07 17:51:46 +0900

    complete_reflectmodule

--- reflect_module.py
+++ reflect_module.py
@@ -1,23 +1,26 @@
 import numpy as np
 import torch
 from scipy.spatial import KDTree
-import matplotlib.pyplot as plt
+from scipy import interpolate
 import math
 from math import pi
 
 
 class reflect:
-    def __init__(self, center_with_direction, range3D, yield_hist = None):
+    def __init__(self, center_with_direction, range3D, InOrOut, yield_hist = None):
+        # center xyz inOrout
         self.center_with_direction = center_with_direction
         # boundary x1x2 y1y2 z1z2
         self.range3D = range3D 
+        # In for +1 out for -1
+        self.InOrOut = InOrOut 
         if yield_hist == None:
             self.yield_hist = np.array([[1.0, 1.05,  1.2,  1.4,  1.5, 1.07, 0.65, 0.28, 0.08,  0], \
-                                        [  0,   10,   20,   30,   40,   50,   60,   70,   80, 90]])
+                                        [  0,   pi/18,   pi/9,   pi/6,   2*pi/9,   5*pi/18,   pi/3,   7*pi/18,   4*pi/9, pi/2]])
         else:
             self.yield_hist = yield_hist
 
-    def scanZ(film):
+    def scanZ(self, film):
         xshape = film.shape[0]
         yshape = film.shape[1]
         zshape = film.shape[2]
@@ -65,7 +68,7 @@ class reflect:
         for c in range(self.center_with_direction.shape[0]):
             sensorcentre = self.center_with_direction[c]
             sensorrange = self.range3D[c]
-
+            sensorInOut = self.InOrOut[c]
             planes_in_range_indice  = np.logical_and(planes[:, 3] < sensorrange[0], planes[:, 3] >= sensorrange[1])
             planes_in_range_indice  |= np.logical_and(planes[:, 4] < sensorrange[2], planes[:, 4] >= sensorrange[3])
             planes_in_range_indice  |= np.logical_and(planes[:, 5] < sensorrange[4], planes[:, 5] >= sensorrange[5])
@@ -85,16 +88,103 @@ class reflect:
                 
                 if (angle >= -pi/2 and angle <= pi/2): # (angle >= -pi/2 and angle <= pi/2):
                     
-                    planes_in_range_consist[i,0] = -planes_in_range[i,0]
-                    planes_in_range_consist[i,1] = -planes_in_range[i,1]
-                    planes_in_range_consist[i,2] = -planes_in_range[i,2]  
+                    planes_in_range_consist[i,0] = -sensorInOut * planes_in_range[i,0]
+                    planes_in_range_consist[i,1] = -sensorInOut * planes_in_range[i,1]
+                    planes_in_range_consist[i,2] = -sensorInOut * planes_in_range[i,2]  
                     
                 else:
                     
-                    planes_in_range_consist[i,0] = planes_in_range[i,0]
-                    planes_in_range_consist[i,1] = planes_in_range[i,1]
-                    planes_in_range_consist[i,2] = planes_in_range[i,2]
+                    planes_in_range_consist[i,0] = sensorInOut * planes_in_range[i,0]
+                    planes_in_range_consist[i,1] = sensorInOut * planes_in_range[i,1]
+                    planes_in_range_consist[i,2] = sensorInOut * planes_in_range[i,2]
                 
             planes_consist.append(planes_in_range_consist)
             
             return np.array(planes_consist)
+        
+    def get_pointcloud(self, film):
+        test = self.scanZ(film)
+
+        points = test.indices().T
+        print(points.shape)
+
+        surface_tree = KDTree(points)
+        dd, ii = surface_tree.query(points, k=10, workers=1)
+
+        pointsNP = points.numpy()
+
+
+        normal_all = np.zeros((ii.shape[0], 3))
+        for i in range(ii.shape[0]):
+            knn_pt = pointsNP[ii[i]]
+            # print(knn_pt.shape)
+            # xmn = np.mean(knn_pt[:,0], axis=1)
+            # ymn = np.mean(knn_pt[:,1], axis=1)
+            # zmn = np.mean(knn_pt[:,2], axis=1)
+            xmn = np.mean(knn_pt[:,0])
+            ymn = np.mean(knn_pt[:,1])
+            zmn = np.mean(knn_pt[:,2])
+
+            c=np.zeros((np.size(knn_pt,0),3))
+
+            c[:,0] = knn_pt[:,0]-xmn
+            c[:,1] = knn_pt[:,1]-ymn
+            c[:,2] = knn_pt[:,2]-zmn
+
+            cov=np.zeros((3,3))    
+
+            cov[0,0] = np.dot(c[:,0],c[:,0])
+            cov[0,1] = np.dot(c[:,0],c[:,1])
+            cov[0,2] = np.dot(c[:,0],c[:,2])
+
+            cov[1,0] = cov[0,1]
+            cov[1,1] = np.dot(c[:,1],c[:,1])
+            cov[1,2] = np.dot(c[:,1],c[:,2])
+
+            cov[2,0] = cov[0,2]
+            cov[2,1] = cov[1,2]
+            cov[2,2] = np.dot(c[:,2],c[:,2])
+
+            "Single value decomposition (SVD)"
+
+            u,s,vh = np.linalg.svd(cov) # U contains the orthonormal eigenvectors and S contains the eigenvectors
+
+            "Selection of minimum eigenvalue"
+
+            minevindex = np.argmin(s)
+
+            "Selection of orthogonal vector corresponing to this eigenvalue --> vector normal to the plane defined by the kpoints"
+
+            normal_vect = u[:,minevindex]
+            normal_all[i, :] = normal_vect
+
+
+        planes=np.zeros((points.shape[0],6))
+        for i in range(points.shape[0]):
+
+            planes[i,0:3] = normal_all[i] #Keep the coordinates of the normal vectors
+            planes[i,3:6] = pointsNP[i] #Keep the coordinates of the centroid
+
+        planes_consist = self.normalconsistency_3D_real(planes)
+
+        return planes_consist[0]
+    
+    def get_inject_theta(self, plane, pos, vel):
+        # plane = self.get_pointcloud(film)
+        plane_point = plane[:, 3:6]
+        normal = plane[:, :3]
+        velocity_normal = np.linalg.norm(vel, axis=1)
+        velocity = np.divide(-vel.T, velocity_normal).T
+        plane_tree = KDTree(plane_point)
+
+        dd, ii = plane_tree.query(pos, k=1, workers=1)
+
+        dot_products = np.einsum('...i,...i->...', velocity, normal[ii])
+        theta = np.arccos(dot_products)
+        return theta
+    
+    def compare_theta(self, theta):
+        yield_func = interpolate.interp1d(self.yield_hist[1], self.yield_hist[0], kind='quadratic')
+        etch_yield = yield_func(theta)
+        return etch_yield
+
