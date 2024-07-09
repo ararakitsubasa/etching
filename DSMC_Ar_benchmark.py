2abadb0143d36f3e682c7d7a7b0368b13d077541
ararakitsubasa <67034838+ararakitsubasa@users.noreply.github.com>
2024-07-09 15:51:43 +0900

    dsmc

--- DSMC_Ar_benchmark.py
+++ DSMC_Ar_benchmark.py
@@ -1,43 +1,42 @@
 import numpy as np
 import time as Time
 from tqdm import tqdm, trange
-from scipy.special import gamma, factorial
+from scipy.special import gamma, factorial, erf
 from scipy.spatial.transform import Rotation as R
+
 import logging
 
 class transport:
-    def __init__(self, boundaryType, maxMove, timeStep, pressure_pa, temperature, cellSize, celllength, chamberSize, DXsec, logname):
-        self.boundaryType = boundaryType # 'wafer' 'SMD'
+    def __init__(self, mirror, maxMove, timeStep, pressure_pa, temperature, cellSize, celllength, chamberSize, DXsec, logname):
+        self.symmetry = mirror
         self.pressure = pressure_pa
         self.T = temperature
         self.N_A = 6.02214076*10**23
         self.R = 8.31446261815324
         self.q = 1.60217663*10**-19
         self.Al_m = 44.803928e-27
-        # self.IonMass = 39.938/(self.N_A*1000) # Argon?
+        self.IonMass = 39.938/(self.N_A*1000)
         self.atomMass = 1.66054e-27
         self.kB = 1.380649e-23
-        self.Al_atom = 26.98
-        self.Ar_atom = 39.95
-        self.Cm_Ar = (2*self.kB*self.T/(self.Ar_atom*self.atomMass) )**0.5 # (2kT/m)**0.5 39.95 for the Ar
         self.mp = 27 # Al
         self.mg = 40 # Ar
+        self.Al_atom = 26.98
+        self.Ar_atom = 39.95
+        self.Ar_radius = 188e-12
+        self.Al_radius = 184e-12
+        self.sigmaT = np.pi*(self.Ar_radius + self.Al_radius)**2/4
+        self.Cm_Ar = (2*self.kB*self.T/(self.Ar_atom*self.atomMass) )**0.5 # (2kT/m)**0.5 39.95 for the Ar
         self.epsilion = 8.85*10**(-12)
+        self.vg = np.sqrt(2*self.kB*self.T/self.IonMass)
         self.ng_pa = self.pressure/(self.R*self.T)*self.N_A
-        self.cellSize_x = cellSize[0]
-        self.cellSize_y = cellSize[1]
-        self.cellSize_z = cellSize[2]
+        self.cellSizeX = cellSize[0]
+        self.cellSizeY = cellSize[1]
+        self.cellSizeZ = cellSize[2]
         self.celllength = celllength
         self.tstep = timeStep
         self.maxMove = maxMove
         self.chamberX = chamberSize[0]
         self.chamberY = chamberSize[1]
-        self.chamberZ = chamberSize[2]
-        self.cellBinsX = np.linspace(self.chamberX[0][0], self.chamberX[0][1], self.cellSize_x)
-        self.cellBinsY = np.linspace(self.chamberY[0][0], self.chamberY[0][1], self.cellSize_y)
-        self.cellBinsZ = np.linspace(self.chamberZ[0][0], self.chamberZ[0][1], self.cellSize_z)
-        self.Fn = 1e5
-        self.sigmaTCR = 1
         self.DXsec = DXsec
         self.depo_pos = []
         # self.colltype_list = []
@@ -72,47 +71,100 @@ class transport:
         
         velosity_matrix = np.array([u, w, v]).T
         return velosity_matrix
+      
+    def boundary(self, pos, vel, i, j, k, weights_arr):
+        # print(pos)
+        pos_cp = np.asarray(pos)
+        vel_cp = np.asarray(vel)
+        weights_arr_cp = np.asarray(weights_arr)
+        i_cp = np.asarray(i)
+        j_cp = np.asarray(j)
+        k_cp = np.asarray(k)
+        cellSize_x_cp = np.asarray(self.cellSizeX) 
+        cellSize_y_cp = np.asarray(self.cellSizeY) 
+        cellSize_z_cp = np.asarray(self.cellSizeZ) 
 
-    def boundary(self, posvel):
+        if self.symmetry == True:
+            indiceXMax = i_cp >= cellSize_x_cp
+            indiceXMin = i_cp < 0
 
-        if self.boundaryType == 'wafer':
-            pos_radius = np.linalg.norm(posvel[:, :2], axis=1)
-            indices = pos_radius >= self.chamberX
-        elif self.boundaryType == 'SMD':
-            indices = np.logical_or(posvel[:, 0] >= self.cellSize_x * self.celllength, posvel[:, 0] <= -self.cellSize_x * self.celllength)
-            indices |= np.logical_or(posvel[:, 1] >= self.cellSize_y * self.celllength, posvel[:, 1] <= -self.cellSize_y * self.celllength)
-        else:
-            print('no boundary')
+            # 使用布尔索引进行调整
+            i_cp[indiceXMax] -= cellSize_x_cp 
+            pos_cp[indiceXMax, 0] -= self.celllength * self.cellSizeX
+
+            i_cp[indiceXMin] += cellSize_x_cp
+            pos_cp[indiceXMin, 0] += self.celllength * self.cellSizeX
+
+            # 检查并调整 j_cp 和对应的 pos_cp
+            indiceYMax = j_cp >= cellSize_y_cp
+            indiceYMin = j_cp < 0
+
+            # 使用布尔索引进行调整
+            j_cp[indiceYMax] -= cellSize_y_cp
+            pos_cp[indiceYMax, 1] -= self.celllength * self.cellSizeY
+
+            j_cp[indiceYMin] += cellSize_y_cp
+            pos_cp[indiceYMin, 1] += self.celllength * self.cellSizeY
+
+            # 检查并调整 j_cp 和对应的 pos_cp
+            indiceZMax = k_cp >= cellSize_z_cp
+            indiceZMin = k_cp < 0
+
+            # 使用布尔索引进行调整
+            k_cp[indiceZMax] -= cellSize_z_cp
+            pos_cp[indiceZMax, 2] -= self.celllength * self.cellSizeZ
+
+            k_cp[indiceZMin] += cellSize_z_cp
+            pos_cp[indiceZMin, 2] += self.celllength * self.cellSizeZ
+
+        indices = np.logical_or(i_cp >= cellSize_x_cp, i_cp < 0)
+        indices |= np.logical_or(j_cp >= cellSize_y_cp, j_cp < 0)
+        indices |= np.logical_or(k_cp >= cellSize_z_cp, k_cp < 0)
 
         if np.any(indices):
-            posvel = posvel[~indices]
+            pos_cp = pos_cp[~indices]
+            vel_cp = vel_cp[~indices]
+            weights_arr_cp = weights_arr_cp[~indices]
+            i_cp = i_cp[~indices]
+            j_cp = j_cp[~indices]
+            k_cp = k_cp[~indices]
 
-        depo_indices = np.logical_or(posvel[:,2] > self.cellSize_z * self.celllength, posvel[:,2] < 0)
-
-        if np.any(depo_indices):
-            self.depo_pos.append(posvel[depo_indices])
-            posvel = posvel[~depo_indices]
-
-        return posvel
+        return pos_cp, vel_cp, i_cp, j_cp, k_cp, weights_arr_cp
     
     # def depo_position(self, posvel):
     #     self.depo_pos.append(posvel)
 
     def getAcc_sparse(self, posvel, tstep):
 
-        i = np.floor((posvel[:, 0]/self.celllength) + 0.5).astype(int)
-        j = np.floor((posvel[:, 1]/self.celllength) + 0.5).astype(int)
-        k = np.floor((posvel[:, 2]/self.celllength) + 0.5).astype(int)
-
-        nC, edges = np.histogramdd(posvel[:, :3], bins = (self.cellBinsX, self.cellBinsY, self.cellBinsZ))
-
         posvelBoundary = self.boundary(posvel)
         posvelAcc = np.zeros_like(posvelBoundary)
         posvelAcc[:, :3] = posvelBoundary[:, 3:] * tstep + posvelBoundary[:, :3]
         posvelAcc[:, 3:] = posvelBoundary[:, 3:]
 
-        return posvelAcc, posvelBoundary, nC
+        return posvelAcc, posvelBoundary
     
+    def getAcc_depo(self, pos, vel, boxsize, tStep, film, weights_arr, depoStep):
+        dx = boxsize
+
+        pos_cp = pos
+        vel_cp = vel
+
+        tStep_cp = tStep
+
+        i = np.floor((pos_cp[:, 0]/dx) + 0.5).astype(int)
+        j = np.floor((pos_cp[:, 1]/dx) + 0.5).astype(int)
+        k = np.floor((pos_cp[:, 2]/dx) + 0.5).astype(int)
+
+        # pos, vel, i, j, k, cellSize_x, cellSize_y, cellSize_z,
+        pos_cp, Nvel_cp, i, j, k, weights_arr = self.boundary(pos_cp, vel_cp, i, j, k, weights_arr)
+        # print(pos_cp)
+        film_depo, pos_cp, Nvel_cp, weights_arr_depo, depo_count, film_max = self.depo_film(film, pos_cp, Nvel_cp, i, j, k, weights_arr, depoStep)
+
+        Npos2_cp = Nvel_cp * tStep_cp + pos_cp
+
+        return np.array([pos_cp, Nvel_cp]), np.array([Npos2_cp, Nvel_cp]), film_depo, weights_arr_depo, depo_count, film_max
+
+
     def diVr_func(self, d_refi, eVr, wi):
         kb = 1.380649e-23
         Tref = 650
@@ -148,39 +200,62 @@ class transport:
         chi = self.DXsec[randChoice]
         return chi
 
-    def newVel_gpu(self, cp, cg):
+    def newVel_gpu(self, v, vMag):
+        # print(v.shape)
+        energy = 0.5*self.Al_m*vMag**2/self.q
+        pN = energy.shape[0]
+        vMagnew = vMag
+        vfilp = np.ones(v.shape[0])
+        filp_indice = np.array(v[:, 0] < 0)
+        vfilp[filp_indice] = -1
+        theta0 = np.arccos(v[:, 2]/vMag)*vfilp
+        phi0= np.arctan(v[:, 1]/v[:, 0])
+        chi = self.DCS_pdf(energy)
+        phi = 2*np.pi*np.random.rand(pN)
+        rotateMat = np.array([np.sin(chi)*np.cos(phi), np.sin(chi)*np.sin(phi), np.cos(chi)])
+        Vrotate = np.multiply(rotateMat, vMagnew).T
+        rz = R.from_matrix(self.rotate_matrix(phi0, theta0))
+        v_rotate = rz.apply(Vrotate)
+        v_rotate = (self.mp*v + self.mg*v_rotate)/(self.mp+self.mg)
+        return v_rotate 
+
+    def newVel_gpu_cr(self, cp):
         N = cp.shape[0]
-        # cg = self.MaxwellMat(N)
+        cg = self.MaxwellMat(N)
         cm = (self.Al_atom*cp + self.Ar_atom*cg)/(self.Al_atom+self.Ar_atom)
         cr = cp - cg
         cr_mag = np.linalg.norm(cr, axis=1)
         eps = np.random.rand(N)*np.pi*2
         coschi = np.random.rand(N)*2 - 1
         sinchi = np.sqrt(1 - coschi*coschi)
-        postCollision_cr = cr_mag*np.array([coschi, sinchi*np.cos(eps),  sinchi*np.sin(eps)]).T
+        # postCollision_cr = cr_mag*np.array([coschi, sinchi*np.cos(eps),  sinchi*np.sin(eps)]).T
+        postCollision_cr = np.array([coschi*cr_mag, sinchi*np.cos(eps)*cr_mag,  sinchi*np.sin(eps)*cr_mag]).T
         cp_p = cm + self.Ar_atom/(self.Al_atom + self.Ar_atom)*postCollision_cr
-        cg_p = cm - self.Al_atom/(self.Al_atom + self.Ar_atom)*postCollision_cr
-        return cp_p, cg_p
-
+        # cg_p = cm - self.Al_atom/(self.Al_atom + self.Ar_atom)*postCollision_cr
+        return cp_p
+    
     def addPtToList(self, pt):
         self.ionPos_list.append(pt)
 
-    def collision(self, prob, KE, vMag, posvelAcc):
+    def collision(self, prob, posvelAcc):
         rand_val = np.random.rand(prob.shape[0])
         indices = np.nonzero(rand_val < prob)[0]
         if indices.size != 0:
-            v3 = self.newVel_gpu(posvelAcc[indices][:, 3:], vMag[indices])
+            # v3 = self.newVel_gpu(posvelAcc[indices][:, 3:], vMag[indices])
+            v3 = self.newVel_gpu_cr(posvelAcc[indices][:, 3:])
             posvelAcc[:, 3:][indices] = v3
             self.addPtToList(posvelAcc[indices][:, :3])
             return posvelAcc, indices.shape[0]
         else:
             return posvelAcc, 0
         
-    def collProb(self, n, KE, delx, nC):
+    def relVel(self, vmag):
+        x = vmag/self.vg
+        return self.vg*((x + 0.5/x)*erf(x) + 1/np.sqrt(np.pi)*np.exp(-x*x))
 
-        selectedPairs = 0.5*nC*(nC-1)*self.Fn*self.sigmaTCR*self.tstep
-        Xsec_interp = np.interp(KE, self.Xsec[:], self.Xsectot[:])
-        sigTot = Xsec_interp
+    def collProb(self, n, KE, delx):
+        # Xsec_interp = np.interp(KE, self.Xsec[:], self.Xsectot[:])
+        sigTot = self.sigmaT
         return 1 - np.exp(-n * sigTot * delx)
     
     def runE(self, p0, v0, time):
@@ -192,16 +267,17 @@ class transport:
         with tqdm(total=100, desc='running', leave=True, ncols=100, unit='B', unit_scale=True) as pbar:
             i = 0
             while t < tmax:
-                posvelAcc, posvelBoundary, nC = self.getAcc_sparse(PosVel, tstep)
+                posvelAcc, posvelBoundary = self.getAcc_sparse(PosVel, tstep)
                 if posvelAcc.shape[0] < int(1e5):
                     break
-                delx = np.linalg.norm(posvelAcc[:, :3] - posvelBoundary[:, :3], axis=1)
+                # delx = np.linalg.norm(posvelAcc[:, :3] - posvelBoundary[:, :3], axis=1)
                 vMag = np.linalg.norm(posvelBoundary[:, 3:], axis=1)
+                delx = self.relVel(vMag)*tstep
                 vMax = vMag.max()
                 KE = 0.5*self.Al_m*vMag**2/self.q
-                prob = self.collProb(self.ng_pa, KE, delx, nC)
+                prob = self.collProb(self.ng_pa, KE, delx)
                 # posvelCopy = np.copy(posvelAcc)
-                posvelAcc, collsionNum = self.collision(prob, KE, vMag, posvelAcc)
+                posvelAcc, collsionNum = self.collision(prob, posvelAcc)
                 # rotateTure = np.allclose(posvelCopy, posvelAcc)
                 t += self.tstep
                 PosVel = posvelAcc
